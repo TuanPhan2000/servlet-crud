f142277340820ff8197071c6666260d88cb19ab4
Your Name <you@example.com>
2022-07-12 09:40:10 +0700

    first commit

---
+++ src/main/java/com/example/servletcrud/dao/UserDAO.java
@@ -0,0 +1,111 @@
+package com.example.servletcrud.dao;
+
+import com.example.servletcrud.model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDAO {
+
+    private static final String INSERT_USERS_SQL = "INSERT INTO users" + "  (name, email, country) VALUES " +
+            " (?, ?, ?);";
+
+    private static final String SELECT_USER_BY_ID = "select id,name,email,country from users where id =?";
+    private static final String SELECT_ALL_USERS = "select * from users";
+    private static final String DELETE_USERS_SQL = "delete from users where id = ?;";
+    private static final String UPDATE_USERS_SQL = "update users set name = ?,email= ?, country =? where id = ?;";
+
+    public UserDAO() {
+    }
+
+    public void insertUser(User user){
+        try{
+
+            Connection connection = com.example.servletcrud.dbutil.Connection.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL);
+
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getEmail());
+            preparedStatement.setString(3, user.getCountry());
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public User selectUser(int id){
+        User user = null;
+        try{
+            Connection connection = com.example.servletcrud.dbutil.Connection.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_USER_BY_ID);
+            preparedStatement.setInt(1, id);
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while (rs.next()){
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                String country = rs.getString("country");
+                user = new User(id, name, email, country);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return user;
+    }
+
+    public List<User> selectAllUsers(){
+        List<User> users = new ArrayList<>();
+
+        try{
+            Connection connection = com.example.servletcrud.dbutil.Connection.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_USERS);
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                String country = rs.getString("country");
+                users.add(new User(id, name, email, country));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return users;
+    }
+
+    public boolean updateUser(User user){
+        boolean rowUpdated;
+        try{
+            Connection connection = com.example.servletcrud.dbutil.Connection.getConnection();
+            PreparedStatement statement = connection.prepareStatement(UPDATE_USERS_SQL);
+            statement.setString(1, user.getName());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, user.getCountry());
+            statement.setInt(4, user.getId());
+
+            rowUpdated = statement.executeUpdate() > 0;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return rowUpdated;
+    }
+
+    public boolean deleteUser(int id){
+        boolean rowDeleted;
+        try{
+            Connection connection = com.example.servletcrud.dbutil.Connection.getConnection();
+            PreparedStatement statement = connection.prepareStatement(DELETE_USERS_SQL);
+            statement.setInt(1, id);
+            rowDeleted = statement.executeUpdate() > 0;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return rowDeleted;
+    }
+
+}
